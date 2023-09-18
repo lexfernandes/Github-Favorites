ec3e9e0f5adfcfa4d95129b658bbd9c73c9faf5b
lexfernandes <lexlopesfernandes@gmail.com>
2023-09-18 15:04:02 -0300

    create delet user

--- js/favorites.js
+++ js/favorites.js
@@ -23,6 +23,12 @@ export class Favorites {
       },
     ]
   }
+
+  delete(user) {
+    const filteredEntries = this.entries
+    .filter(entry => entry.login !== user.login)
+    console.log(filteredEntries)
+  }
 }
 //classe que vai criar a visualização e eventos do HTML
 export class FavoritesView extends Favorites {
@@ -41,7 +47,12 @@ export class FavoritesView extends Favorites {
       row.querySelector('.user span').textContent = user.login
       row.querySelector('.repositories').textContent = user.public_repos
       row.querySelector('.followers').textContent = user.followers
-
+      row.querySelector('.remove').onclick = () => {
+        const isOk = confirm('Tem certeza que deseja deletar essa linha?')
+        if(isOk){
+          this.delete(user)
+        }
+      }
 
       this.tbody.append(row)
     })
