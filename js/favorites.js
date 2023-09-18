4df9f3bafd543390e43a38f660574d156c343a09
lexfernandes <lexlopesfernandes@gmail.com>
2023-09-18 13:41:26 -0300

    remove data tr

--- js/favorites.js
+++ js/favorites.js
@@ -5,7 +5,6 @@ export class Favorites {
     this.root = document.querySelector(root)
   }
 }
-
 //classe que vai criar a visualização e eventos do HTML
 export class FavoritesView extends Favorites {
   constructor(root) {
@@ -15,7 +14,41 @@ export class FavoritesView extends Favorites {
   // Função roda a função removeAllTr
   update() {
     this.removeAllTr()
+    [
+      {
+        login: "lexfernandes",
+        name: "Lex Fernandes",
+        public_repos: "76",
+        followers: "2000",
+      }
+      {
+        login: "maykbrito",
+        name: "Mayk Brito",
+        public_repos: "80",
+        followers: "1200",
+      }
+    ]
   }
+  createRow() {
+    const tr = document.createElement("tr")
+    tr.innerHTML = `
+            <td class="users">
+              <img
+                src="https://github.com/lexfernandes.png"
+                alt="Imagem do Alex"
+              />
+              <a href="https://github.com/lexfernandes" target="_blank"
+                ><p>Alex Fernandes</p>
+                <span>lexfernandes</span>
+              </a>
+            </td>
+            <td class="repositories">76</td>
+            <td class="Followers">9589</td>
+            <td><button class="remove">&times;</button></td>
+          `
+    return tr
+  }
+
   // remove a listagem ao atualizar a página.(Função)
   removeAllTr() {
     const tbody = this.root.querySelector("table tbody")
