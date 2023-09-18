1810e74c377828a69a1a7138e6692c0303ca3e96
lexfernandes <lexlopesfernandes@gmail.com>
2023-09-18 16:54:17 -0300

    create endpoint

--- js/favorites.js
+++ js/favorites.js
@@ -1,3 +1,20 @@
+export class GithubUser {
+  static search (username) {
+    const endpoint = `https://api.github.com/users/${username}`
+    return fetch(endpoint)
+    .then( data => data.json())
+    .then(({ login, name, public_repos, followers}) => ({
+      login, 
+      name,
+      public_repos,
+      followers
+    }))
+      
+    }
+}
+
+
+
 // classe que vai conter a lógica dos dados
 // como os dados serão estruturados
 export class Favorites {
